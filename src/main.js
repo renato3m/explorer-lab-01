06049f7d2d453c8cbb6520250926a34f37073ba9
Renato Moreira <renatomvboas@gmail.com>
2022-10-19 10:30:11 -0300

    feat: Mudando a cor e logo do cartão via javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./css/index.css"
+
+const ccBgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#Df6F29", "#C69347"],
+    default: ["black", "gary"],
+  }
+
+  ccBgColor1.setAttribute("fill", colors[type][0])
+  ccBgColor2.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
